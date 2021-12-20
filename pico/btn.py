498bd183210e052bfbe6a1ce7797f736c980a1fd
Alan Christie <alan.christie@matildapeak.com>
2021-12-20 12:48:50 +0000

    Experiment with GitHub actions
    Use of pylint and mypy

--- pico/btn.py
+++ pico/btn.py
@@ -1,20 +1,22 @@
 """A simple program that connects the explorer buttons to a callback.
-It uses a sinple outer-loop to act on button presses and enact a debounce,
-This keeps the callcak (ISR) short and simple.
+It uses a simple outer-loop to act on button presses and enact a debounce,
+This keeps the callback (ISR) short and simple.
 """
 
-from machine import Pin
-import utime
+# pylint: disable=import-error, global-statement
+
+from machine import Pin  # type: ignore
+import utime  # type: ignore
 
 # A global boolean variable,
 # one for each explorer button.
-btn_a_request = False
-btn_b_request = False
-btn_x_request = False
-btn_y_request = False
+_BTN_A_REQUEST: bool = False
+_BTN_B_REQUEST: bool = False
+_BTN_X_REQUEST: bool = False
+_BTN_Y_REQUEST: bool = False
 
 
-def btn_callback(pin):
+def _btn_callback(pin):
     """The button callback (ISR).
 
     Here we set the corresponding button request
@@ -22,45 +24,45 @@ def btn_callback(pin):
     It's up to an 'outer loop' to run the actions
     for the button.
     """
-    global btn_a_request
-    global btn_b_request
-    global btn_x_request
-    global btn_y_request
+    global _BTN_A_REQUEST
+    global _BTN_B_REQUEST
+    global _BTN_X_REQUEST
+    global _BTN_Y_REQUEST
 
     # What GPIO pin is this?
     # We have to interrogate the string representation!
-    pin_id = int(str(pin)[4:-1].split(',')[0])
+    pin_id: int = int(str(pin)[4:-1].split(',')[0])
     if pin_id == 12:
-        btn_a_request = True
+        _BTN_A_REQUEST = True
     elif pin_id == 13:
-        btn_b_request = True
+        _BTN_B_REQUEST = True
     elif pin_id == 14:
-        btn_x_request = True
+        _BTN_X_REQUEST = True
     elif pin_id == 15:
-        btn_y_request = True
+        _BTN_Y_REQUEST = True
 
 
-def btn():
-    global btn_a_request
-    global btn_b_request
-    global btn_x_request
-    global btn_y_request
+def _btn():
+    global _BTN_A_REQUEST
+    global _BTN_B_REQUEST
+    global _BTN_X_REQUEST
+    global _BTN_Y_REQUEST
 
     # Register each of the explorer buttons.
     # Pressing any of them runs the btn_callback() function.
     print('Registering buttons...')
 
-    btn_a = Pin(12, Pin.IN, Pin.PULL_UP)
-    btn_a.irq(trigger=Pin.IRQ_FALLING, handler=btn_callback)
+    btn_a: Pin = Pin(12, Pin.IN, Pin.PULL_UP)
+    btn_a.irq(trigger=Pin.IRQ_FALLING, handler=_btn_callback)
 
-    btn_b = Pin(13, Pin.IN, Pin.PULL_UP)
-    btn_b.irq(trigger=Pin.IRQ_FALLING, handler=btn_callback)
+    btn_b: Pin = Pin(13, Pin.IN, Pin.PULL_UP)
+    btn_b.irq(trigger=Pin.IRQ_FALLING, handler=_btn_callback)
 
-    btn_x = Pin(14, Pin.IN, Pin.PULL_UP)
-    btn_x.irq(trigger=Pin.IRQ_FALLING, handler=btn_callback)
+    btn_x: Pin = Pin(14, Pin.IN, Pin.PULL_UP)
+    btn_x.irq(trigger=Pin.IRQ_FALLING, handler=_btn_callback)
 
-    btn_y = Pin(15, Pin.IN, Pin.PULL_UP)
-    btn_y.irq(trigger=Pin.IRQ_FALLING, handler=btn_callback)
+    btn_y: Pin = Pin(15, Pin.IN, Pin.PULL_UP)
+    btn_y.irq(trigger=Pin.IRQ_FALLING, handler=_btn_callback)
 
     print('Registered')
 
@@ -68,51 +70,51 @@ def btn():
     # If it's set then act on it (here we just print the button's name)
     # then, after a short delay (to also de-bounce the button),
     # clear the request.
-    
+
     print('Listening...')
 
     while True:
-    
+
         # Flags, set if we've seen the request for a button.
         # This is used to reset the request after a short delay
-        seen_a = False
-        seen_b = False
-        seen_x = False
-        seen_y = False
+        seen_a: bool = False
+        seen_b: bool = False
+        seen_x: bool = False
+        seen_y: bool = False
         # Has any button been pressed?
         # Start each new set of actions with a delimiter ('-').
-        if btn_a_request or btn_b_request or btn_x_request or btn_y_request:
+        if _BTN_A_REQUEST or _BTN_B_REQUEST or _BTN_X_REQUEST or _BTN_Y_REQUEST:
             print('-')
         # Act on each button
-        if btn_a_request:
+        if _BTN_A_REQUEST:
             print('A')
             seen_a = True
-        if btn_b_request:
+        if _BTN_B_REQUEST:
             print('B')
             seen_b = True
-        if btn_x_request:
+        if _BTN_X_REQUEST:
             print('X')
             seen_x = True
-        if btn_y_request:
+        if _BTN_Y_REQUEST:
             print('Y')
             seen_y = True
 
         # De-bounce delay
         utime.sleep_ms(1000)
-        
+
         # Reset any button we've seen.
         # And print a symbol indicating button hadling is complete.
         if seen_a:
-            btn_a_request = False
+            _BTN_A_REQUEST = False
         if seen_b:
-            btn_b_request = False
+            _BTN_B_REQUEST = False
         if seen_x:
-            btn_x_request = False
+            _BTN_X_REQUEST = False
         if seen_y:
-            btn_y_request = False
+            _BTN_Y_REQUEST = False
         if seen_a or seen_b or seen_x or seen_y:
             print('*')
 
 
 if __name__ == '__main__':
-    btn()
+    _btn()
